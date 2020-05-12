e2a21d741cd8cebc1f0fcecf68431d14a01a5cb0
Leon Jamison <leonjamison@icloud.com>
2020-05-12 17:42:12 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,35 @@
 // Code ClickityClick Component Here
+import React from 'react';
+ 
+class LightSwitch extends React.Component {
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      toggled: false
+    };
+  }
+ 
+  handleClick = () => {
+    // Update our state here...
+    this.setState(previousState => {
+        return {
+            toggled:!previousState.toggled
+        }
+
+    })
+    
+  };
+ 
+  render() {
+    return (
+      <div>
+        {/* <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p> */}
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON": "OFF"}</button>
+      </div>
+    );
+  }
+}
+ 
+export default LightSwitch;
